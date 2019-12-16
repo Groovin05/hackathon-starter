bf117600bd376a30480a799d93a2f88c8e24b551
Elizabeth Munsinger <emunsinger@gmail.com>
2019-12-15 19:20:06 -0600

    updating .travis.yml

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function main() {
     ssh
       .connect({
         // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-        host: '54.242.73.244',
+        host: '3.88.159.126',
         username: 'ubuntu',
         privateKey: 'hs-key.pem'
       })
